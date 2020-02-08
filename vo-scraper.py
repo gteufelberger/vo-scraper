7d2cc9f901d46d06b63f55fff6ab30068c66603a
Georg Teufelberger <tgeorg@student.ethz.ch>
2020-02-08 17:35:23 +0100

    Remove leftover TODO

--- vo-scraper.py
+++ vo-scraper.py
@@ -56,7 +56,6 @@ series_metadata_suffix = ".series-metadata.json"
 video_info_prefix = "https://video.ethz.ch/.episode-video.json?recordId="
 directory_prefix = "Lecture Recordings/"
 
-#TODO link gitlab issue page
 gitlab_issue_page = "https://gitlab.ethz.ch/tgeorg/vo-scraper/issues"
 
 video_quality = "high"
